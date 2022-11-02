adab7eb405393a059a67ef02465a3d9e9c8da98d
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-11-02 18:20:46 -0400

    Remove softwareTagPattern variable - won't be initialized correctly for first runs

--- update.ps1
+++ update.ps1
@@ -5,7 +5,6 @@ $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
 
 $legalPath = Join-Path -Path $currentPath -ChildPath 'legal'
 $softwareRepo = 'LiveSplit/LiveSplit'
-$softwareTagPattern = "$($Latest.SoftwareVersion)"
 
 function global:au_GetLatest {
     $version = Get-LatestStableVersion
@@ -28,7 +27,7 @@ function global:au_BeforeUpdate ($Package)  {
 }
 
 function global:au_AfterUpdate ($Package)  {
-    $licenseUri = "https://raw.githubusercontent.com/$($softwareRepo)/$softwareTagPattern/LICENSE"
+    $licenseUri = "https://raw.githubusercontent.com/$($softwareRepo)/$($Latest.SoftwareVersion)/LICENSE"
     $licenseContents = Invoke-WebRequest -Uri $licenseUri -UseBasicParsing
 
     $licensePath = Join-Path -Path $legalPath -ChildPath 'LICENSE.txt'
@@ -39,15 +38,15 @@ function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
             "<packageSourceUrl>[^<]*</packageSourceUrl>" = "<packageSourceUrl>https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)</packageSourceUrl>"
-            "<licenseUrl>[^<]*</licenseUrl>" = "<licenseUrl>https://github.com/$($softwareRepo)/blob/$($softwareTagPattern)/LICENSE</licenseUrl>"
-            "<projectSourceUrl>[^<]*</projectSourceUrl>" = "<projectSourceUrl>https://github.com/$($softwareRepo)/tree/$($softwareTagPattern)</projectSourceUrl>"
-            "<releaseNotes>[^<]*</releaseNotes>" = "<releaseNotes>https://github.com/$($softwareRepo)/releases/tag/$($softwareTagPattern)</releaseNotes>"
+            "<licenseUrl>[^<]*</licenseUrl>" = "<licenseUrl>https://github.com/$($softwareRepo)/blob/$($Latest.SoftwareVersion)/LICENSE</licenseUrl>"
+            "<projectSourceUrl>[^<]*</projectSourceUrl>" = "<projectSourceUrl>https://github.com/$($softwareRepo)/tree/$($Latest.SoftwareVersion)</projectSourceUrl>"
+            "<releaseNotes>[^<]*</releaseNotes>" = "<releaseNotes>https://github.com/$($softwareRepo)/releases/tag/$($Latest.SoftwareVersion)</releaseNotes>"
             "<copyright>[^<]*</copyright>" = "<copyright>Copyright (c) 2013-$(Get-Date -Format yyyy) Christopher Serr and Sergey Papushin</copyright>"
         }
         'legal\VERIFICATION.txt' = @{
             '%checksumValue%' = "$($Latest.Checksum32)"
             '%checksumType%' = "$($Latest.ChecksumType32.ToUpper())"
-            '%tagReleaseUrl%' = "https://github.com/$($softwareRepo)/releases/tag/$($softwareTagPattern)"
+            '%tagReleaseUrl%' = "https://github.com/$($softwareRepo)/releases/tag/$($Latest.SoftwareVersion)"
             '%archiveUrl%' = "$($Latest.Url32)"
             '%archiveFileName%' = "$($Latest.FileName32)"
         }
