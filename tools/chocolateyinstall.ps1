04fd2ab54758f19b01a322b41ee81067b0fecbda
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-11-02 18:21:04 -0400

    Update to 1.8.23

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$archiveFileName = 'LiveSplit_1.8.22.zip'
+$archiveFileName = 'LiveSplit_1.8.23.zip'
 $archiveFilePath = Join-Path -Path $toolsDir -ChildPath $archiveFileName
 
 $packageArgs = @{
